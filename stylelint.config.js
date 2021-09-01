d3cc1fe1318131cb6a0ce66062ba7928867d5714
Vincent Hardouin <vincenthardouin@icloud.com>
2021-09-01 14:18:42 +0200

    Install stylelint-scss and configure it

--- stylelint.config.js
+++ stylelint.config.js
@@ -3,7 +3,11 @@ module.exports = {
     'stylelint-config-standard',
     'stylelint-config-prettier'
   ],
+  plugins: ['stylelint-scss'],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
-  rules: {}
+  rules: {
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': true,
+  }
 }
