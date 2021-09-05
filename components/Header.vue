84ff3891f36440b40e3d37e110602c8efc9e77d5
Vincent Hardouin <vincenthardouin@icloud.com>
2021-09-05 16:54:51 +0200

    Improve responsive for Header

--- components/Header.vue
+++ components/Header.vue
@@ -17,24 +17,30 @@
 
 <style lang='scss' scoped>
 .header {
-  max-width: 820px;
-  margin: 0 auto;
+  width: 100%;
   padding: 14px;
   height: 80px;
   display: flex;
   align-items: center;
+  gap: 14px;
 
   &__title {
     font-family: $font-open-sans;
-    font-size: 1.44rem;
     color: $cloud-burst;
     text-decoration: none;
+    text-align: center;
+    font-size: 1.1rem;
+
+    @include device-is('tablet') {
+      font-size: 1.44rem;
+    }
   }
 }
 
 .navbar {
   display: flex;
   list-style-type: none;
+  padding: 0;
 
   li {
     margin: 6px;
