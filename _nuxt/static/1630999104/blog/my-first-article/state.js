a1ccadddcc095d2c1d507e3c311f0bdc19e45671
VincentHardouin <VincentHardouin@users.noreply.github.com>
2021-09-07 07:18:28 +0000

    deploy: fa1089c7552ea2436374f546de11f547ceaa94d9

---
+++ _nuxt/static/1630999104/blog/my-first-article/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1630999104",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fmy-first-article",config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},strapi:{url:"https:\u002F\u002Fstrapi.vincenthardouin.dev"},http:{browserBaseURL:b}}}}(null,"\u002F"));
