7f928a8fb6b28326dc5834b0908931040359e0b5
VincentHardouin <VincentHardouin@users.noreply.github.com>
2021-09-16 21:03:13 +0000

    deploy: 981f315e398cd8afa556ef002ccc9623c97f2324

--- _nuxt/static/1631550774/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1631550774",layout:"default",error:b,serverRendered:true,routePath:a,config:{_app:{basePath:a,assetsPath:"\u002F_nuxt\u002F",cdnURL:b},strapi:{url:"https:\u002F\u002Fstrapi.vincenthardouin.dev"},http:{browserBaseURL:a}}}}("\u002F",null));
