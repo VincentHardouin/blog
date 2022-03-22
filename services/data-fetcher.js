a8844c460e7fef1f75a639fe020cd46d49d4479a
Vincent Hardouin <vincenthardouin@icloud.com>
2022-03-22 09:45:07 +0100

    feat(seo): extract seo construction from data-fetcher

--- services/data-fetcher.js
+++ services/data-fetcher.js
@@ -1,23 +1,10 @@
-import { getMetaTags } from '../utils/seo';
+import { getAllMeta } from '../utils/seo';
 
 export async function getArticle({ $strapi, params }) {
-  const matchingArticles = await $strapi.$articles.find({
+  const [article] = await $strapi.$articles.find({
     slug: params.slug,
   });
-  const article = matchingArticles[0];
-
-  const seo = {
-    siteName: 'Vincent Hardouin',
-    metaTitle: article.title,
-    metaDescription: article.description,
-    imageUrl: article.seoImageUrl,
-  };
-  const meta = {
-    titleTemplate: `%s | ${seo.siteName}`,
-    title: seo.metaTitle,
-    meta: getMetaTags(seo),
-  };
-
+  const meta = getAllMeta(article);
   return { article, meta };
 }
 
@@ -38,18 +25,7 @@ export async function getPage({ $strapi, params }) {
   const [page] = await $strapi.$pages.find({
     slug: params.slug,
   });
-  const seo = {
-    siteName: 'Vincent Hardouin',
-    metaTitle: page.title,
-    imageUrl: page.seoImageUrl,
-  };
-
-  const meta = {
-    titleTemplate: `%s | ${seo.siteName}`,
-    title: seo.metaTitle,
-    meta: getMetaTags(seo),
-  };
-
+  const meta = getAllMeta(page);
   return {
     page,
     meta,
