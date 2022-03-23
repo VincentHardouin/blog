118b37346f027f844ca96ddf616be2f003874b3e
Vincent Hardouin <vincenthardouin@icloud.com>
2022-03-23 15:01:24 +0100

    chore: install color-mode

--- nuxt.config.js
+++ nuxt.config.js
@@ -55,6 +55,7 @@ export default {
     // https://go.nuxtjs.dev/stylelint
     '@nuxtjs/stylelint-module',
     ['@nuxtjs/style-resources', { scss: ['assets/scss/globals.scss'] }],
+    '@nuxtjs/color-mode',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
